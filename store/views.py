5ac819cfe4c99eaee9c650285c7af8df2fd6ead2
lmamu21 <lmamu21@freeuni.edu.ge>
2024-10-05 14:58:36 +0400

    added two views (main page and specific item by id, simplified and randomized) in store app

--- store/views.py
+++ store/views.py
@@ -1,3 +1,27 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.defaulttags import lorem
+import faker
+import json
+
+categories = ["electronics", "home accessories", "clothing", "books", "groceries"]
+
 
 # Create your views here.
+def index(request):
+    result = {"count": 245,
+              "categories": categories
+              }
+
+    return HttpResponse(json.dumps(result))
+
+
+def item(request, item_id):
+    result = {}
+    fake = faker.Faker()
+    result["price"] = fake.pricetag()
+    result["category"] = random.choice(categories)
+    result["item"] = fake.word()
+    return HttpResponse(json.dumps(result))
