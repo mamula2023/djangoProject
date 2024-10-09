295cd72e7a13b7bbee12a98da75f581e81912788
lmamu21 <lmamu21@freeuni.edu.ge>
2024-10-10 03:12:38 +0400

    created basic models for Product and Category

--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, default='category')
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    categories = models.ManyToManyField(Category, related_name='categories')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
