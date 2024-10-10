81c8f7430cf8575a8f7a362d1a1a6746fd29d4c1
lmamu21 <lmamu21@freeuni.edu.ge>
2024-10-10 16:30:43 +0400

    removed redundant urls

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('item/<int:item_id>/', views.item, name='item'),
     path('categories/', views.categories, name='categories'),
     path('products/', views.products, name='products')
 ]
