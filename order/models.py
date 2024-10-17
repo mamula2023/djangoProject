3f7771bc98377f5f5380661c016bfdc246e13120
lmamu21 <lmamu21@freeuni.edu.ge>
2024-10-17 14:37:28 +0400

    new UserCart model for each user

--- order/models.py
+++ order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+import custom_user
+
+
 # Create your models here.
+
+class UserCart(models.Model):
+    user = models.OneToOneField(custom_user, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
